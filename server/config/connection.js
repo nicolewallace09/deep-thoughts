defcf8a028eae12413a2084f089d65d966b82f04
nicolewallace09 <nicole.elisaw@gmail.com>
2021-10-12 10:24:33 -0700

    moving mlab to atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect('mongodb+srv://nicole_w:lokiandlily2020@cluster0.hfj3i.mongodb.net/deep-thoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
